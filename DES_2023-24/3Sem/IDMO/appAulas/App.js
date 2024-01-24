e13bdc3263b26d75aa7e239c7bbc123f89daef97
izaiasmaia <professor.senai.izaias@gmail.com>
2024-01-24 17:25:49 -0300

    Aula 01

--- DES_2023-24/3Sem/IDMO/appAulas/App.js
+++ DES_2023-24/3Sem/IDMO/appAulas/App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/** Aula 01: Introdução ao React-Native */
 
+import { StatusBar } from 'expo-status-bar';
+/** Import dos componentes que estão sendo utilizados 
+ * StyleSheet: permite criar os estilos do nosso app;
+ * Text: cria um texto;
+ * View: Representa todo o layout do nosso app;
+*/
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+/** Função principal do aplicativo */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá mundo!</Text>
+      <Text style={styles.titulo}>Olá mundo!</Text>
       <Text>Meu primeiro app mobile!!!</Text>
+      <Button title='Clique aqui' onPress={() => Alert.alert('Seja bem vindo!')}></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/** Estilização dos componentes, definimos o estilo para cada componente individualmente */
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#ff00ff',
+    backgroundColor: 'purple',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    color: 'blue',
+  }
 });
